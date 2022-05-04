75234d6055d807850e2b5eb5a369d2913a9d08cb
ageorge0368 <98937102+ageorgestony@users.noreply.github.com>
2022-05-04 19:26:08 -0400

    malloc for maze (not done)

--- maze.c
+++ maze.c
@@ -18,7 +18,14 @@ int main() {
 	
 	printf("(%i, %i)\n%i %i", starting_x, starting_y, x_dim, y_dim);
 	
-
+	char actual_maze[0][0];
+	
+	for (i = 0; i < x_dim; i++) {
+		for (j = 0; j < y_dim; j++) {
+			actual_maze = (char*) malloc (y_dim * sizeof(char));
+		}
+		actual_maze = (char*) malloc (x_dim * sizeof(char));
+	}
 
 
   return 0;
