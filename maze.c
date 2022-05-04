55456596d74956df9b2a426e14c6d2bf036d125e
ageorge0368 <ageorge0368@gmail.com>
2022-05-04 17:05:40 -0400

    can read starting and dims

--- maze.c
+++ maze.c
@@ -1,5 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-  printf("Hello world");
-  print("Testing\n");
+	FILE *maze; 
+	
+	if ((maze = fopen("maze.txt", "r")) == NULL) {
+		printf("error in opening maze.txt");
+		exit(1);
+	} 
+
+	int i;
+	int starting_x, starting_y;
+	int x_dim, y_dim;
+	
+	fscanf(maze, "(%i, %i)", &starting_x, &starting_y);
+	fscanf(maze, "%i %i", &x_dim, &y_dim);
+	
+	printf("(%i, %i)\n%i %i", starting_x, starting_y, x_dim, y_dim);
+	
+
+
+
   return 0;
 }
